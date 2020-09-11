5a6fc5899dbdf07d1ac31ee697af4f4a5cd8f88c
Feiyang Fan <feiyangfan1@gmail.com>
2020-09-11 17:59:18 -0400

    commit all files

--- index.js
+++ index.js
@@ -11,29 +11,29 @@ app.use(cors());
 app.use(express.json());
 app.use(express.static("./public"));
 
-app.get("/", (req, res) => {
-  res.json({
-    message: "message",
-  });
-});
+// app.get("/", (req, res) => {
+//   res.json({
+//     message: "message",
+//   });
+// });
 
-app.get("/url/:id", (req, res) => {
-  res.json({
-    message: "get a short url by id",
-  });
-});
+// app.get("/url/:id", (req, res) => {
+//   res.json({
+//     message: "get a short url by id",
+//   });
+// });
 
-app.get("/:id", (req, res) => {
-  res.json({
-    message: "redirect to url ",
-  });
-});
+// app.get("/:id", (req, res) => {
+//   res.json({
+//     message: "redirect to url ",
+//   });
+// });
 
-app.post("/url", (req, res) => {
-  res.json({
-    message: "create a short url",
-  });
-});
+// app.post("/url", (req, res) => {
+//   res.json({
+//     message: "create a short url",
+//   });
+// });
 
 const port = process.env.PORT || 8000;
 app.listen(port, () => {
